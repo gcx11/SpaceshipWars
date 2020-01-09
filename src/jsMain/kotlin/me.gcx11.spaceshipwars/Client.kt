7b931334cf544907a3b51e50d8ec96445e65f9af
gcx11 <gcx11@seznam.cz>
2020-01-09 20:37:18 +0100

    Add websockets

--- src/jsMain/kotlin/me.gcx11.spaceshipwars/Client.kt
+++ src/jsMain/kotlin/me.gcx11.spaceshipwars/Client.kt
@@ -1,9 +1,47 @@
 package me.gcx11.spaceshipwars
 
-import kotlin.browser.*
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.js.Js
+import io.ktor.client.features.websocket.WebSockets
+import io.ktor.client.features.websocket.ws
+import io.ktor.http.HttpMethod
+import io.ktor.http.cio.websocket.*
+import io.ktor.util.KtorExperimentalAPI
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlin.browser.document
 
+@KtorExperimentalAPI
 fun main() {
     document.addEventListener("DOMContentLoaded", {
-        println(message)
+        val client = HttpClient(Js) {
+            install(WebSockets)
+        }
+
+        GlobalScope.launch {
+            client.ws(
+                method = HttpMethod.Get,
+                host = "127.0.0.1",
+                port = 8080, path = "/ws"
+            ) {
+                outgoing.send(Frame.Text("1"))
+
+                loop@ for (frame in incoming) {
+                    when (frame) {
+                        is Frame.Text -> {
+                            var number = frame.readText().toInt()
+                            println("Received from server: $number")
+
+                            if (number > 1024) {
+                                close(CloseReason(CloseReason.Codes.NORMAL, "Bye"))
+                                break@loop
+                            }
+
+                            outgoing.send(Frame.Text("${++number}"))
+                        }
+                    }
+                }
+            }
+        }
     })
 }
--- src/jvmMain/kotlin/me/gcx11/spaceshipwars/Server.kt
+++ src/jvmMain/kotlin/me/gcx11/spaceshipwars/Server.kt
@@ -1,17 +1,26 @@
 package me.gcx11.spaceshipwars
 
 import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.html.respondHtml
+import io.ktor.http.cio.websocket.CloseReason
+import io.ktor.http.cio.websocket.Frame
+import io.ktor.http.cio.websocket.close
+import io.ktor.http.cio.websocket.readText
 import io.ktor.http.content.resource
 import io.ktor.http.content.static
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import io.ktor.websocket.WebSockets
+import io.ktor.websocket.webSocket
 import kotlinx.html.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
+        install(WebSockets)
+
         routing {
             get("/") {
                 call.respondHtml {
@@ -24,6 +33,25 @@ fun main() {
                     }
                 }
             }
+
+            webSocket("/ws") {
+                loop@ for (frame in incoming) {
+                    when (frame) {
+                        is Frame.Text -> {
+                            var number = frame.readText().toInt()
+                            println("Received from client: $number")
+
+                            if (number > 1024) {
+                                close(CloseReason(CloseReason.Codes.NORMAL, "Bye"))
+                                break@loop
+                            }
+
+                            outgoing.send(Frame.Text("${++number}"))
+                        }
+                    }
+                }
+            }
+
             static("/static") {
                 resource("SpaceshipWars.js")
             }
